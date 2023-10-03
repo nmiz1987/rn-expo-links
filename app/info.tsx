760bbf9da1a420820dfc777c73f96df5e5ac297e
Netanel Mizrahi <nmiz1987@gmail.com>
2023-10-03 22:17:22 +0300

    init boilerplate

---
+++ app/info.tsx
@@ -0,0 +1,10 @@
+import { Link } from "expo-router";
+import { View, Text } from "react-native";
+
+export default function Page() {
+  return (
+    <View>
+      <Text>info</Text>
+    </View>
+  );
+}
